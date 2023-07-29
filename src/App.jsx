e0a2b5aee0d65b36431bb33437ac6717ba29ad26
KQuiggins <kashfamilyvalues@gmail.com>
2023-07-29 13:34:29 -0400

    added context to feedback stats

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 import AboutIconLink from "./components/AboutIconLink";
 import FeedbackData from "./data/FeedbackData.js";
 
@@ -26,30 +27,29 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Header />
+      <FeedbackProvider>
+        <Router>
+          <Header />
 
-        <div className="container">
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={
-                <>
-                  <FeedbackForm handleAdd={addFeedback} />
-                  <FeedbackStats feedback={feedback} />
-                  <FeedbackList
-                    feedback={feedback}
-                    handleDelete={deleteFeedback}
-                  />
-                </>
-              }
-            ></Route>
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-          <AboutIconLink />
-        </div>
-      </Router>
+          <div className="container">
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <>
+                    <FeedbackForm handleAdd={addFeedback} />
+                    <FeedbackStats />
+                    <FeedbackList handleDelete={deleteFeedback} />
+                  </>
+                }
+              ></Route>
+              <Route path="/about" element={<AboutPage />} />
+            </Routes>
+            <AboutIconLink />
+          </div>
+        </Router>
+      </FeedbackProvider>
     </>
   );
 }
